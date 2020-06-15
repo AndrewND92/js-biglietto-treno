dce4d7a3d51b902b67c9d473354fa08bec1a0f54
Andrea <miranda.andrea.nd@gmail.com>
2020-06-15 15:45:27 +0200

    calcolo prezzo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// bligietto del Treno
+
+var prezzo = 0.21;
+
+// chiedere al cliente quanti km vuole fare
+var km = prompt("Quanti km vuoi percorrere?")
+console.log(km);
+
+// chiedere l'età
+var eta = prompt("quanti anni hai?")
+console.log(eta);
+
+// colcolo del prezzo al km
+var calcoloPrezzoKm = 0.21 * km;
+console.log(calcoloPrezzoKm);
